fbbd044e057e625e90cb8bd4a29e61b1035dd809
shot <shakido@mail.ru>
2022-07-16 13:53:54 +0300

    add

--- src/app.js
+++ src/app.js
@@ -13,3 +13,63 @@ $(document).click( function(event){
     e.target.style.height = 'auto'
     e.target.style.height = e.target.scrollHeight + 2 + "px"
   });
+
+  function proverka(input) {
+    input.value = input.value.replace(/[',", А-я]/, '');
+    
+    return false;
+  };
+
+  function checkInf(){
+    const inputsInf = document.querySelectorAll('input');
+    const ageInf = document.getElementById('age');
+    const nameInf = document.getElementById("name")
+    const maleInf = document.getElementById("male");
+    const localCountInf = document.getElementById("local");
+    const naziInf = document.getElementById("nazi");
+    const idskinInf = document.getElementById("idskin");
+    const kventaPers = document.getElementById("kventapers")
+
+    if(ageInf.value == ''){
+        ageInf.style.borderBottom = '2px solid #9f2525';
+    }else{
+        ageInf.style.border = '';
+    }
+
+    if(nameInf.value == ''){
+        nameInf.style.borderBottom = '2px solid #9f2525';
+    }else{
+        nameInf.style.border = '';
+    }
+
+    if(maleInf.value == ''){
+        maleInf.style.borderBottom = '2px solid #9f2525';
+    }else{
+        maleInf.style.border = '';
+    }
+
+    if(localCountInf.value == ''){
+        localCountInf.style.borderBottom = '2px solid #9f2525';
+    }else{
+        localCountInf.style.border = '';
+    }
+
+    if(naziInf.value == ''){
+        naziInf.style.borderBottom = '2px solid #9f2525';
+    }else{
+        naziInf.style.border = '';
+    }
+
+    if(idskinInf.value == ''){
+        idskinInf.style.borderBottom = '2px solid #9f2525';
+    }else{
+        idskinInf.style.border = '';
+    }
+
+    if(kventaPers.value == ''){
+        kventaPers.style.borderBottom = '2px solid #9f2525';
+    }else{
+        kventaPers.style.border = '';
+    }
+
+  }
