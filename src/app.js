38a2745bc4f22aff766de4ae543d47910f4a060f
shot <shakido@mail.ru>
2022-07-16 13:00:38 +0300

    re

--- src/app.js
+++ src/app.js
@@ -8,3 +8,8 @@ $(document).click( function(event){
     $("#burger-menu").hide();
     event.stopPropagation();
   });
+
+  document.querySelector('textarea').addEventListener('input', function (e) {
+    e.target.style.height = 'auto'
+    e.target.style.height = e.target.scrollHeight + 2 + "px"
+  });
